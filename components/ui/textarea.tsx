aef38e7b742802d4d3c5db1c221207de7de14773
Neil Mahajan <neilsmahajan@gmail.com>
2025-01-26 19:43:59 -0700

    refactor: add PropTypes to Textarea component and import React in Skeleton; remove unused Table component

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import PropTypes from 'prop-types';
 
 import { cn } from "@/lib/utils";
 
@@ -18,5 +19,8 @@ const Textarea = React.forwardRef<
   );
 });
 Textarea.displayName = "Textarea";
+Textarea.propTypes = {
+  className: PropTypes.string,
+};
 
 export { Textarea };
