117eeaedb6ed4dae4fd62c6f97ee16535b1814b2
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-02 15:08:41 -0700

    feat: enhance Google Analytics analysis by updating metrics and dimensions for improved reporting

--- functions/google_analytics/analyze_google_analytics.py
+++ functions/google_analytics/analyze_google_analytics.py
@@ -1,12 +1,13 @@
 from flask import jsonify, request, make_response
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import DateRange, Metric, Dimension, RunReportRequest
-from google.oauth2.service_account import Credentials
-from dotenv import load_dotenv, dotenv_values
+from google.oauth2 import service_account
 import os
+from dotenv import load_dotenv
 
-# Set the environment variable for Google Application Credentials
+# Load environment variables from .env file
 load_dotenv()
+
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 
 def analyze_google_analytics(req):
@@ -18,14 +19,25 @@ def analyze_google_analytics(req):
     if not property_id or not start_date or not end_date:
         return make_response(jsonify({"error": "propertyID, startDate, and endDate are required"}), 400)
 
-    credentials = Credentials.from_service_account_file(os.getenv("GOOGLE_SERVICE_ACCOUNT_CREDENTIALS"), scopes=SCOPES)
+    # Load credentials from the environment variable
+    credentials = service_account.Credentials.from_service_account_file(
+        os.getenv("GOOGLE_SERVICE_ACCOUNT_CREDENTIALS"),
+        scopes=SCOPES
+    )
+
     client = BetaAnalyticsDataClient(credentials=credentials)
 
     request = RunReportRequest(
         property=f"properties/{property_id}",
         date_ranges=[DateRange(start_date=start_date, end_date=end_date)],
-        metrics=[Metric(name="activeUsers")],
-        dimensions=[Dimension(name="country")]
+        dimensions=[
+            Dimension(name="date"),
+        ],
+        metrics=[
+            Metric(name="sessions"),
+            Metric(name="bounceRate"),
+            Metric(name="keyEvents"),
+        ]
     )
 
     response = client.run_report(request)
@@ -33,8 +45,10 @@ def analyze_google_analytics(req):
     result = {
         "rows": [
             {
-                "country": row.dimension_values[0].value,
-                "activeUsers": row.metric_values[0].value,
+                "date": row.dimension_values[0].value,
+                "sessions": row.metric_values[0].value,
+                "bounceRate": row.metric_values[1].value,
+                "keyEvents": row.metric_values[2].value,
             }
             for row in response.rows
         ]
