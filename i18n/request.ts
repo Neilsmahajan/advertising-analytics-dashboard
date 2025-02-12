2bc2bdc21ddaa75bbef936b630546004a86b4385
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-12 05:25:37 -0700

    feat: enhance locale detection in request configuration based on accept-language header

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,4 +1,5 @@
 import { getRequestConfig } from "next-intl/server";
+import { headers } from "next/headers";
 import { Locale, routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
@@ -7,7 +8,18 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   // Ensure that a valid locale is used
   if (!locale || !routing.locales.includes(locale as Locale)) {
-    locale = routing.defaultLocale;
+    const headersList = await headers();
+    const acceptLanguage = headersList.get("accept-language");
+    if (acceptLanguage) {
+      const preferredLocale = acceptLanguage.split(",")[0].split("-")[0];
+      if (routing.locales.includes(preferredLocale as Locale)) {
+        locale = preferredLocale;
+      } else {
+        locale = routing.defaultLocale;
+      }
+    } else {
+      locale = routing.defaultLocale;
+    }
   }
 
   return {
