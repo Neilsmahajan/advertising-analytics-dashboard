494a4cad0305b9d51d01c5b7b92501a960fc55b8
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-12 05:45:58 -0700

    feat: add internationalization for "Explore Services" button in English and French

--- app/[locale]/scroll-to-supported-platforms.tsx
+++ app/[locale]/scroll-to-supported-platforms.tsx
@@ -2,12 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import React from "react";
+import { useTranslations } from "next-intl";
 
 /**
  *
  * @constructor
  */
 export default function ScrollToSupportedPlatforms() {
+  const t = useTranslations("Home.buttons");
   const scrollToSupportedPlatforms = () => {
     const element = document.getElementById("supported-platforms");
     if (element) {
@@ -22,7 +24,7 @@ export default function ScrollToSupportedPlatforms() {
       className="text-white bg-[#0077be] hover:bg-[#005f9e]"
       onClick={scrollToSupportedPlatforms}
     >
-      EXPLORE SERVICES
+      {t("exploreServices")}
     </Button>
   );
 }
