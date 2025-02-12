fb8a79ffb84dc81f05231330f5f6b3e425018455
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-12 05:43:36 -0700

    feat: add internationalization for sign-up button in English and French

--- app/[locale]/sign-up-button.tsx
+++ app/[locale]/sign-up-button.tsx
@@ -6,12 +6,14 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useRouter } from "@/i18n/routing";
 import { auth } from "@/lib/firebaseConfig";
 import React from "react";
+import { useTranslations } from "next-intl";
 
 /**
  *
  * @constructor
  */
 export default function SignUpButton() {
+  const t = useTranslations("Home.buttons");
   const [user] = useAuthState(auth);
   const provider = new GoogleAuthProvider();
   const router = useRouter();
@@ -33,7 +35,7 @@ export default function SignUpButton() {
       className="text-white bg-[#0077be] hover:bg-[#005f9e]"
       onClick={user ? handleRedirect : handleSignIn}
     >
-      {user ? "VIEW YOUR QUERIES" : "SIGN UP FOR FREE"}
+      {user ? t("viewYourQueries") : t("signUpForFree")}
     </Button>
   );
 }
