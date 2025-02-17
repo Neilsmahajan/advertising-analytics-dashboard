49f0027bfdf5fe6b89de1c494780f2255749aca5
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-17 16:00:45 -0700

    feat: implement Microsoft Ads authentication functionality and refactor related components

--- functions/main.py
+++ functions/main.py
@@ -20,7 +20,9 @@ from google_analytics.analyze_google_analytics import analyze_google_analytics
 from generate_report.generate_report import generate_report
 from meta_ads.analyze_meta_ads import analyze_meta_ads
 from send_email.send_email import send_email
-from microsoft_ads.analyze_microsoft_ads import analyze_microsoft_ads
+from microsoft_ads.authenticate_microsoft_ads.authenticate_microsoft_ads import (
+    authenticate_microsoft_ads,
+)
 
 
 # Define HTTP functions
@@ -113,7 +115,7 @@ def contact_form_function(req: https_fn.Request) -> https_fn.Response:
 
 
 @https_fn.on_request()
-def analyze_microsoft_ads_function(req: https_fn.Request) -> https_fn.Response:
+def authenticate_microsoft_ads_function(req: https_fn.Request) -> https_fn.Response:
     # Handle preflight requests
     if request.method == "OPTIONS":
         response = make_response()
@@ -123,6 +125,6 @@ def analyze_microsoft_ads_function(req: https_fn.Request) -> https_fn.Response:
         return response
 
     # Handle actual request
-    response = analyze_microsoft_ads(req)
+    response = authenticate_microsoft_ads(req)
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
