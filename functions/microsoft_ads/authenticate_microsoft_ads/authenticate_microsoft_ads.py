3379c675e65305cfb16c5f750cb7da3ae79650e8
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-21 16:49:05 -0700

    feat: enhance Microsoft Ads authentication with OAuthWebAuthCodeGrant and web browser integration

--- functions/microsoft_ads/authenticate_microsoft_ads/authenticate_microsoft_ads.py
+++ functions/microsoft_ads/authenticate_microsoft_ads/authenticate_microsoft_ads.py
@@ -1,11 +1,16 @@
 import os, sys
-from bingads.authorization import AuthorizationData
+from bingads.authorization import AuthorizationData, OAuthWebAuthCodeGrant
+import webbrowser
+from flask import jsonify
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-from auth_helper import (
-    authenticate,
-    DEVELOPER_TOKEN,
-)
+
+DEVELOPER_TOKEN = os.getenv("MICROSOFT_ADS_DEVELOPER_TOKEN")  # Your developer token
+ENVIRONMENT = "production"  # Change to 'production' for live data
+CLIENT_ID = os.getenv("MICROSOFT_ADS_CLIENT_ID")  # Your new client ID
+CLIENT_SECRET = os.getenv("MICROSOFT_ADS_CLIENT_SECRET")  # Your client secret
+REDIRECTION_URI = "http://localhost:3000/en/microsoft-ads"
+CLIENT_STATE = "ClientStateGoesHere"
 
 
 def authenticate_microsoft_ads(req):
@@ -16,4 +21,16 @@ def authenticate_microsoft_ads(req):
         authentication=None,
     )
 
-    authenticate(authorization_data)
+    authentication = OAuthWebAuthCodeGrant(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,  # Add client secret here
+        redirection_uri=REDIRECTION_URI,  # Add redirection URI here
+        env=ENVIRONMENT,
+    )
+    authentication.state = CLIENT_STATE
+    authorization_data.authentication = authentication
+    webbrowser.open(
+        authorization_data.authentication.get_authorization_endpoint(), new=1
+    )
+
+    return jsonify({"status": "success", "message": "Authentication initiated"})
