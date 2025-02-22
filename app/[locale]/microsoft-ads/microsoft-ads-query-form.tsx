a2d9ca0253b6a504585bd8a4c70280ee33568a90
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-22 12:46:11 -0700

    fix: correct typo in Microsoft Ads authentication function URL and clean up import statements

--- app/[locale]/microsoft-ads/microsoft-ads-query-form.tsx
+++ app/[locale]/microsoft-ads/microsoft-ads-query-form.tsx
@@ -63,7 +63,7 @@ export default function MicrosoftAdsQueryForm() {
 
   const handleAuthenticate = async () => {
     await axios.post(
-      // "https://us-central1-advertisinganalytics-dashboard.cloudfunctions.net/authenticate_microsoft_ads_function",
+      // "https://us-central1-advertisinganalytics-dashboard.cloudfunctions.net/authenticate_microsoft_ads_funciton",
       "http://127.0.0.1:5001/advertisinganalytics-dashboard/us-central1/authenticate_microsoft_ads_function",
       {},
     );
