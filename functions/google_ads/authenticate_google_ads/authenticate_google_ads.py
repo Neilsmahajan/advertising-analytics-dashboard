beb0e1231c7cc1191e3e0d153988c77862502de8
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-25 15:16:10 -0700

    feat: implement Google Ads authentication functionality

---
+++ functions/google_ads/authenticate_google_ads/authenticate_google_ads.py
@@ -0,0 +1,39 @@
+import argparse
+import hashlib
+import os
+import re
+import socket
+import sys
+from urllib.parse import unquote
+from google_auth_oauthlib.flow import Flow
+
+_SCOPE = "https://www.googleapis.com/auth/adwords"
+# _SERVER = "127.0.0.1"
+_SERVER = "localhost"
+# _PORT = 8080
+_PORT = 3000
+_REDIRECT_URI = f"http://{_SERVER}:{_PORT}/en/google-ads"
+
+def authenticate_google_ads():
+    # oauth 2 google ads credentials file path: functions/credentials/oauth_2_google_ads_client_id_and_secret.json and current file: functions/google_ads/authenticate_google_ads/authenticate_google_ads.py
+    client_secrets_path = os.path.join(
+        os.path.dirname(__file__), "..", "..", "credentials", "oauth_2_google_ads_client_id_and_secret.json"
+    )
+    scopes = [_SCOPE]
+    flow = Flow.from_client_secrets_file(client_secrets_path, scopes=scopes)
+    flow.redirect_uri = _REDIRECT_URI
+    passthrough_val = hashlib.sha256(os.urandom(1024)).hexdigest()
+
+    authorization_url, state = flow.authorization_url(
+        access_type="offline",
+        state=passthrough_val,
+        prompt="consent",
+        include_granted_scopes="true",
+    )
+    print("Paste this URL into your browser: ")
+    print(authorization_url)
+    print(f"\nWaiting for authorization and callback to: {_REDIRECT_URI}")
+
+
+if __name__ == "__main__":
+    authenticate_google_ads()
