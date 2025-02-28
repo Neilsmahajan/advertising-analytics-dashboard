1326114fe6a169ffe507174fca21ef917faca3b8
Neil Mahajan <neilsmahajan@gmail.com>
2025-02-28 12:49:02 -0700

    feat: add Google Ads client initialization and campaign fetching functionality

--- functions/google_ads/analyze_google_ads/analyze_google_ads.py
+++ functions/google_ads/analyze_google_ads/analyze_google_ads.py
@@ -1,7 +1,8 @@
 import os
-from flask import jsonify
+from flask import json, jsonify
 from urllib.parse import urlparse, parse_qs
 
+from google.ads.googleads.client import GoogleAdsClient
 from google_auth_oauthlib.flow import Flow
 
 _SCOPE = [
@@ -15,6 +16,7 @@ _SERVER = "localhost"
 # _PORT = 8080
 _PORT = 3000
 _REDIRECT_URI = f"http://{_SERVER}:{_PORT}/en/google-ads"
+DEVELOPER_TOKEN = os.getenv("GOOGLE_ADS_DEVELOPER_TOKEN")  # Your developer token
 
 
 def analyze_google_ads(req):
@@ -57,6 +59,21 @@ def fetch_google_ads_data(customer_id, response_uri):
     flow.fetch_token(code=code, include_client_id=True)
     refresh_token = flow.credentials.refresh_token
     print(f"Refresh token: {refresh_token}")
+    with open(client_secrets_path, "r") as f:
+        client_secrets = json.load(f)
+    client_id = client_secrets["web"]["client_id"]
+    client_secret = client_secrets["web"]["client_secret"]
+
+    credentials = {
+        "developer_token": DEVELOPER_TOKEN,
+        "refresh_token": refresh_token,
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "use_proto_plus": True,
+    }
+
+    googleads_client = GoogleAdsClient.load_from_dict(credentials)
+    print("Campaigns: ", get_campaigns(googleads_client, customer_id))
 
     # Placeholder for fetching Google Ads data
     return {
@@ -67,3 +84,27 @@ def fetch_google_ads_data(customer_id, response_uri):
         "conversions": 1,
         "engagements": 1,
     }
+
+
+def get_campaigns(client, customer_id):
+    ga_service = client.get_service("GoogleAdsService")
+
+    query = """
+        SELECT
+          campaign.id,
+          campaign.name
+        FROM campaign
+        ORDER BY campaign.id"""
+
+    # Issues a search request using streaming.
+    stream = ga_service.search_stream(customer_id=customer_id, query=query)
+    campaigns = []
+    for batch in stream:
+        for row in batch.results:
+            print(
+                f"Campaign with ID {row.campaign.id} and name "
+                f'"{row.campaign.name}" was found.'
+            )
+            campaigns.append({"id": row.campaign.id, "name": row.campaign.name})
+            # [END get_campaigns]
+    return campaigns
