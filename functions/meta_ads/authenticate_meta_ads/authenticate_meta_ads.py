4463d253a8f8750d1804f38ce7fff4ee53eb5316
Neil Mahajan <neilsmahajan@gmail.com>
2025-03-14 11:35:26 -0700

    feat: enhance Meta Ads authentication with OAuth URL generation and error handling

--- functions/meta_ads/authenticate_meta_ads/authenticate_meta_ads.py
+++ functions/meta_ads/authenticate_meta_ads/authenticate_meta_ads.py
@@ -6,9 +6,20 @@ def authenticate_meta_ads(req):
     data = req.get_json() or {}
     lang = data.get("lang", "en")
     redirection_uri = (
-        "https://advertisinganalyticsdashboard.com/fr/meta-ads"
+        "https://advertisinganalyticsdashboard.com/fr/meta-ads/"
         # "http://localhost:3000/fr/meta-ads"
         if lang == "fr"
-        else "https://advertisinganalyticsdashboard.com/en/meta-ads"
+        else "https://advertisinganalyticsdashboard.com/en/meta-ads/"
         # "http://localhost:3000/en/meta-ads"
     )
+    try:
+        url = (
+            f"https://www.facebook.com/v22.0/dialog/oauth"
+            f"?client_id=1578493349517464"
+            f"&redirect_uri=https://advertisinganalyticsdashboard.com/en/meta-ads/"
+            f"&scope=ads_read"
+        )
+        response = {"url": url}
+        return jsonify(response), 200
+    except Exception as e:
+        return {"error": str(e)}
